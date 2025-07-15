187438ceaa31e2bed3b082bab78ab52dff18b143
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-15 19:05:06 +0000

    fix

--- src/Navbar.vue
+++ src/Navbar.vue
@@ -1,17 +1,16 @@
 <template>
 	<BNavbar
-		toggleable="md"
-		:type="variant"
-		:variant="color"
+		:variant="props.variant"
+		:bg="props.color"
 		class="px-5"
 		sticky="top"
 	>
-		<BNavbarBrand :to="{ name: logoHref }">{{ logoText }}</BNavbarBrand>
+		<BNavbarBrand :to="{ name: props.logoHref }">{{ props.logoText }}</BNavbarBrand>
 		<BNavbarToggle target="nav-collapse" />
 		<BCollapse id="nav-collapse" is-nav>
 			<BNavbarNav class="ms-auto text-end">
 				<BNavItem
-					v-for="link in navLinks"
+					v-for="link in props.navLinks"
 					:key="link.href"
 					:to="{ name: link.href }"
 					>{{ link.text }}
@@ -21,30 +20,27 @@
 	</BNavbar>
 </template>
 
-<script>
-import { inject } from 'vue';
-
-// Regular script block to define options
-export default {
-  props: {
-    logoText: { type: String, default: "My Site" },
-    logoHref: { type: String, default: "Home" },
-    navLinks: { type: Array, default: () => [] },
-    variant: { type: String, default: "dark" },
-    color: { type: String, default: "primary" },
-    base: {
-      type: String,
-      default: inject('config', {}).baseUrl // Access config here
-    },
-  },
-};
-</script>
-
 <script setup>
+import { inject, computed } from 'vue';
+
 import { BNavbar, BNavbarBrand, BNavbarToggle, BCollapse, BNav, BNavItem } from "bootstrap-vue-next";
 
+const config = inject('config', {});
 
+const props = defineProps({
+  logoText: { type: String, default: "My Site" },
+  logoHref: { type: String, default: "Home" },
+  navLinks: { type: Array, default: () => [] },
+  variant: { type: String, default: "dark" },
+  color: { type: String, default: "primary" },
+  base: {
+    type: String,
+    // Use a function for the default value
+    default: () => inject('config', {}).baseUrl
+  },
+});
 
+// You can now use props.base in your template or other logic
 </script>
 
 
