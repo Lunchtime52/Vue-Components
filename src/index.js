bf74b18e79c8e3d4afbb3e7ddb18e40d75925a23
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-15 18:15:09 +0000

    atuomate componet registry

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 // src/index.js
 
-// --- MANUAL COMPONENT IMPORTS ---
-// 1. Manually import each component from your library here.
-import Footer from './Footer.vue';
-import Hero from './Hero.vue';
+// --- AUTOMATED COMPONENT IMPORTS ---
+// Use require.context to automatically import all .vue files
+const components = require.context('/src', false, /\.vue$/);
 
 // 2. CREATE THE PLUGIN'S INSTALL METHOD
 const install = (app) => {
   // This plugin's only job is to register its own components.
   // The main application will be responsible for installing BootstrapVue.
-  app.component('Footer', Footer);
-  app.component('Hero', Hero);
+  components.keys().forEach(fileName => {
+    const componentConfig = components(fileName);
+    const componentName = fileName
+      .replace(/^\.\//, '')
+      .replace(/\.\w+$/, '');
+    app.component(componentName, componentConfig.default || componentConfig);
+  });
 };
 
 // 3. EXPORT THE PLUGIN
 const plugin = {
   install,
 };
-
 export default plugin;
