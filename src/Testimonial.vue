65d36f2031973390c8646eb912c37ca287defa22
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-15 20:50:44 +0000

    update

--- src/Testimonial.vue
+++ src/Testimonial.vue
@@ -1,7 +1,7 @@
 <template>
     <BContainer class="section-padding">
       <div class="bg-light p-5 rounded-lg shadow-md d-flex flex-column align-items-center">
-        <img v-if="image" :src="image" :alt="author" class="w-25 h-25 rounded-full object-cover mb-4" />
+        <img v-if="image" :src="image" :alt="author" class="w-15 h-15 rounded-full object-cover mb-4" />
         <blockquote class="text-secondary text-center mb-4">
           <p class="italic">"{{ quote }}"</p>
         </blockquote>
