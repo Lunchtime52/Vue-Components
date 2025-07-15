a03b955dbf38730f4b51a843dba8251225541a33
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-15 18:27:55 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@
 
 // --- AUTOMATED COMPONENT IMPORTS ---
 // Use require.context to automatically import all .vue files
-const components = import.meta.glob('./*.vue');
+const components = import.meta.glob("./*.vue");
 
-// 2. CREATE THE PLUGIN'S INSTALL METHOD
-const install = (app) => {
+const install = async (app) => {
+  // Make the install function async
   // This plugin's only job is to register its own components.
   // The main application will be responsible for installing BootstrapVue.
+  const registrationPromises = [];
+
   for (const fileName in components) {
     const componentConfig = components[fileName]; // Get the function that imports the module
     const componentName = fileName
-      .replace(/^.\//, '') // Remove "./"
-      .replace(/\.vue$/, '');
-    componentConfig().then(module => {
+      .replace(/^.\//, "") // Remove "./"
+      .replace(/\.vue$/, "");
+    const registrationPromise = componentConfig().then((module) => {
+      // Create a promise for each component registration
       app.component(componentName, module.default || module);
     });
+    registrationPromises.push(registrationPromise);
   }
+
+  await Promise.all(registrationPromises); // Wait for all components to be registered
 };
 
 // 3. EXPORT THE PLUGIN
