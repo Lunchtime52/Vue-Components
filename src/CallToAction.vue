1d7c337f1ac1bb4a456e06911cc69d58dc9ad72a
ryanbuchanan52.tn <ryanbuchanan52.tn@gmail.com>
2025-07-15 18:53:16 +0000

    dependecy

--- src/CallToAction.vue
+++ src/CallToAction.vue
@@ -8,26 +8,25 @@
     </div>
   </template>
   
-  <script>
-  export default {
-    name: 'CallToAction',
-    props: {
-      title: {
-        type: String,
-        default: 'Sign up today!',
-      },
-      description: {
-        type: String,
-        default: 'Join our community and start learning',
-      },
-      buttonText: {
-        type: String,
-        default: 'Sign up',
-      },
-      buttonLink: {
-        type: String,
-        default: 'Contact',
-      },
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    title: {
+      type: String,
+      default: 'Sign up today!',
     },
-  };
+    description: {
+      type: String,
+      default: 'Join our community and start learning',
+    },
+    buttonText: {
+      type: String,
+      default: 'Sign up',
+    },
+    buttonLink: {
+      type: String,
+      default: 'Contact',
+    },
+  });
   </script>
